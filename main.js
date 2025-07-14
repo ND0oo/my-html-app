ffd87362c36e0caa9eac936c12911ad19c747f3d
ND0oo <nd16@qq.com>
2025-07-14 19:13:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: true
     }
   })
-  
-  win.loadFile('QtScrcpy按键映射编辑器-02.html')
+
+  // 修复路径问题
+  win.loadFile(path.join(__dirname, 'editor.html'))
 }
 
 app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
