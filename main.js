e99b7352bc2065a41128cd7c51ae7bc582471b8e
ND0oo <nd16@qq.com>
2025-07-14 20:54:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'icon.ico'), // 添加这行
+    title: "QtScrcpy映射编辑器 v2.0",
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
-  win.loadFile('editor.html')
+  win.loadFile(path.join(__dirname, 'editor.html'))
 }
 
 app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
